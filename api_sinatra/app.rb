39d7ff4e814672f892a250d0997b74fe62d93527
OpenShift guest <kzintiempire@gmail.com>
2013-07-27 23:56:46 -0400

    sinatra json api hello world

---
+++ api_sinatra/app.rb
@@ -0,0 +1,131 @@
+# Require the bundler gem and then call Bundler.require to load in all gems
+# listed in Gemfile.
+require 'bundler'
+Bundler.require
+
+# Setup DataMapper with a database URL. On Heroku, ENV['DATABASE_URL'] will be
+# set, when working locally this line will fall back to using SQLite in the
+# current directory.
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite")
+
+# Define a simple DataMapper model.
+class Thing
+  include DataMapper::Resource
+
+  property :id, Serial, :key => true
+  property :created_at, DateTime
+  property :title, String, :length => 255
+  property :description, Text
+end
+
+class Vehicle
+  include DataMapper::Resource
+
+  property :id, Serial, :key => true
+  property :type, String
+end
+  
+
+# Finalize the DataMapper models.
+DataMapper.finalize
+
+# Tell DataMapper to update the database according to the definitions above.
+DataMapper.auto_upgrade!
+
+get '/' do
+  send_file './public/index.html'
+end
+
+# Route to show all Things, ordered like a blog
+get '/things' do
+  content_type :json
+  @things = Thing.all(:order => :created_at.desc)
+
+  @things.to_json
+end
+
+get '/vehicles' do
+  content_type :json
+  @vehicles = Vehicle.all
+
+  @vehicles.to_json
+end
+
+# CREATE: Route to create a new Thing
+post '/things' do
+  content_type :json
+
+  # These next commented lines are for if you are using Backbone.js
+  # JSON is sent in the body of the http request. We need to parse the body
+  # from a string into JSON
+  # params_json = JSON.parse(request.body.read)
+
+  # If you are using jQuery's ajax functions, the data goes through in the
+  # params.
+  @thing = Thing.new(params)
+
+  if @thing.save
+    @thing.to_json
+  else
+    halt 500
+  end
+end
+
+# READ: Route to show a specific Thing based on its `id`
+get '/things/:id' do
+  content_type :json
+  @thing = Thing.get(params[:id])
+
+  if @thing
+    @thing.to_json
+  else
+    halt 404
+  end
+end
+
+# UPDATE: Route to update a Thing
+put '/things/:id' do
+  content_type :json
+
+  # These next commented lines are for if you are using Backbone.js
+  # JSON is sent in the body of the http request. We need to parse the body
+  # from a string into JSON
+  # params_json = JSON.parse(request.body.read)
+
+  # If you are using jQuery's ajax functions, the data goes through in the
+  # params.
+
+  @thing = Thing.get(params[:id])
+  @thing.update(params)
+
+  if @thing.save
+    @thing.to_json
+  else
+    halt 500
+  end
+end
+
+# DELETE: Route to delete a Thing
+delete '/things/:id/delete' do
+  content_type :json
+  @thing = Thing.get(params[:id])
+
+  if @thing.destroy
+    {:success => "ok"}.to_json
+  else
+    halt 500
+  end
+end
+
+# If there are no Things in the database, add a few.
+if Thing.count == 0
+  Thing.create(:title => "Test Thing One", :description => "Sometimes I eat pizza.")
+  Thing.create(:title => "Test Thing Two", :description => "Other times I eat cookies.")
+end
+if Vehicle.count == 0
+  3.times do 
+    Vehicle.create(:type => "Snow Plow")
+    Vehicle.create(:type => "Street Sweeper")
+    Vehicle.create(:type => "Garbage Truck")
+  end
+end
---
+++ api_sinatra/readme.md
@@ -0,0 +1,9 @@
+# Sinatra JSON API Example
+
+This is a very small example app showing how to accept and return JSON as an
+API. An app like this could serve as a back end for a mobile app or a
+[Backbone](http://backbonejs.org) app, or any other web interface that relies
+on AJAX requests to view and change data.
+
+If you're new to Sinatra I'd recommend starting with my [Sinatra Heroku
+Template](https://github.com/stevenklise/Sinatra-Heroku-Template) repository.
